dd5971a12a438d5160795f5cdc038629e3cd33e5
weijordan <12012972+weijordan@user.noreply.gitee.com>
2024-11-12 16:32:26 +0800

    deploy修改无缓存

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,20 @@
-#srv=blog_plus-boot
+#!/bin/bash
+
+# 设置服务名称变量
 srv=blog_plus
-echo"项目名为$srv"
+
+# 输出项目名称
+echo "项目名为$srv"
+
+# 停止并删除容器
 docker stop $srv
 docker rm -f $srv
+
+# 删除镜像
 docker rmi -f $srv
-decker build -t $srv.
+
+# 无缓存构建 Docker 镜像
+docker build --no-cache -t $srv .
+
+# 使用 Docker Compose 部署服务
 docker-compose -f ~/DockerCompose/$srv/docker-compose.yml up -d
