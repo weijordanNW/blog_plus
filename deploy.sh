9164f9dabf1246d4a4df8800a33f1cc4229621d0
weijordan <12012972+weijordan@user.noreply.gitee.com>
2024-11-13 14:34:14 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,26 @@
-container_name="blog_plus"
+#!/bin/bash
+
+# 设置服务名称变量
+srv=blog_plus
 
 echo "Docker 部署中..."
 
-docker stop $container_name
-docker rm -f $container_name
-docker rmi -f $container_name
+# 输出项目名称
+echo "项目名为$srv"
+
+# 停止并删除容器
+docker stop $srv
+docker rm -f $srv
+
+# 删除镜像
+docker rmi -f $srv
 
 echo '创建容器并将其启用'
 
-#  进入项目的根目录执行
-#  -t 后面是镜像名，需要小写
-#  -f 后面为 docker 构建所依赖的 Dockerfile 文件的路径
-#  最后的 . 表示将当前目录作为构建上下文
-docker build -t $container_name -f /docker-opt/$container_name/app/docker/Dockerfile .
+# 无缓存构建 Docker 镜像
+docker build --no-cache -t $srv .
 
-#  还是在项目的根目录执行
-#  -f 后面为 docker-compose 命令所依赖的配置文件路径
-docker-compose -f /docker-opt/$container_name/app/docker/docker-compose.yml up -d
+# 使用 Docker Compose 部署服务
+docker-compose -f ~/DockerCompose/$srv/docker-compose.yml up -d
 
 echo '容器部署成功'
